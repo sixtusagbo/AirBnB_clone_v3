3ffa5c54f21dd7da63d5d4abbfdb6df9b6a9e281
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-24 23:27:05 +0100

    Retrieve single State object

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''Handles all RESTful API actions for `State` objects'''
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, abort
 from models import storage
 from models.state import State
 
@@ -12,3 +12,10 @@ def states():
     for value in storage.all(State).values():
         result.append(value.to_dict())
     return jsonify(result)
+
+@app_views.route('/states/<state_id>')
+def state(state_id):
+    result = storage.get(State, state_id)
+    if (result is None):
+        abort(404)
+    return jsonify(result.to_dict())
