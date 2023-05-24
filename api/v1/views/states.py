5c835797b6fc5423b1dd0e515397006fabf81b43
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-25 00:24:18 +0100

    Delete a State object

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,21 +1,49 @@
 #!/usr/bin/python3
-'''Handles all RESTful API actions for `State` objects'''
+"""Handles all RESTful API actions for `State` objects"""
 from api.v1.views import app_views
 from flask import jsonify, abort
 from models import storage
 from models.state import State
 
-@app_views.route('/states')
+
+@app_views.route("/states")
 def states():
-    '''Retrieve the list of all `State` objects'''
+    """Retrieve the list of all `State` objects"""
     result = []
     for value in storage.all(State).values():
         result.append(value.to_dict())
     return jsonify(result)
 
-@app_views.route('/states/<state_id>')
-def state(state_id):
+
+@app_views.route("/states/<state_id>")
+def state(state_id: str):
+    """Retrive one state object
+
+    Args:
+        state_id (string): state identifier
+
+    Returns:
+        Response: `State` object in json
+    """
     result = storage.get(State, state_id)
-    if (result is None):
+    if result is None:
         abort(404)
     return jsonify(result.to_dict())
+
+
+@app_views.route("/states/<state_id>", methods=["DELETE"])
+def delete_state(state_id):
+    """Delete a state object
+
+    Args:
+        state_id (str): state identifier
+
+    Returns:
+        Response: Empty dictionary - `{}`
+    """
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    state.delete()
+    storage.save()
+    return jsonify({})
