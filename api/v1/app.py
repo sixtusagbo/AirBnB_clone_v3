ac944fc56aba0a9111a3ea152f914423f90cb1bb
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-25 01:27:08 +0100

    Pycodesctyle compliant

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,19 +11,20 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
-host = getenv('HBNB_API_HOST', '0.0.0.0')
-port = getenv('HBNB_API_PORT', '5000')
+host = getenv("HBNB_API_HOST", "0.0.0.0")
+port = getenv("HBNB_API_PORT", "5000")
 
 
 @app.teardown_appcontext
 def teardown(exception):
-    '''Cleanup operations'''
+    """Cleanup operations"""
     storage.close()
+
 
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify({'error': 'Not Found'}), 404)
+    return jsonify({"error": "Not Found"}), 404
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host, port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,25 +12,27 @@ from models import storage
 
 
 classes = {
-  "amenities": Amenity, 
-  "cities": City, 
-  "places": Place, 
-  "reviews": Review, 
-  "states": State, 
-  "users": User
+    "amenities": Amenity,
+    "cities": City,
+    "places": Place,
+    "reviews": Review,
+    "states": State,
+    "users": User,
 }
 
-@app_views.route('/status')
-def status():
-    '''Status of my API'''
-    return jsonify({'status': 'ok'})
 
-@app_views.route('/stats')
+@app_views.route("/status")
+def status():
+    """Status of my API"""
+    return jsonify({"status": "ok"})
+
+
+@app_views.route("/stats")
 def stats():
-    '''Retrieve the number of each objects by type'''
+    """Retrieve the number of each objects by type"""
     statistics = {}
 
     for key, value in classes.items():
         statistics[key] = storage.count(value)
-    
+
     return jsonify(statistics)
