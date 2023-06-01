2f89ec1f555e188c70d49464a1374b72c706efc0
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-06-01 21:35:41 +0100

    Get one amenity

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -2,7 +2,7 @@ from api.v1.views import app_views
 from models import storage
 from models.amenity import Amenity
 
-from flask import jsonify
+from flask import jsonify, abort
 
 
 @app_views.route("/amenities")
@@ -10,7 +10,7 @@ def amenities():
     """Retrieve list of all `Amenity` objects
 
     Returns:
-        `~flask.Response`: List of all the amenities
+        `flask.Response`: List of all the amenities
     """
     amenities = storage.all(Amenity)
     result = []
@@ -19,3 +19,20 @@ def amenities():
         result.append(amenity.to_dict())
 
     return jsonify(result)
+
+
+@app_views.route("/amenities/<amenity_id>")
+def amenity(amenity_id):
+    """Retrieve one `Amenity`
+
+    Args:
+        amenity_id (str): Amenity identifier
+
+    Returns:
+        flask.Response: An amenity in json
+    """
+    amenity = storage.get(Amenity, amenity_id)
+    if not amenity:
+        abort(404)
+
+    return jsonify(amenity.to_dict())
