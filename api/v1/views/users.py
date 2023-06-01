05bf8e4cf1ffbb378a346f30969b11a4282fc354
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-06-01 23:39:18 +0100

    Create user

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -35,3 +35,31 @@ def one_user(user_id):
         abort(404)
 
     return jsonify(user.to_dict())
+
+
+@app_views.route("/users/<user_id>", methods=["DELETE"])
+def delete_user(user_id):
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+
+    user.delete()
+    storage.save()
+
+    return jsonify({})
+
+
+@app_views.route("/users", methods=["POST"])
+def create_user():
+    payload = request.get_json()
+    if not payload:
+        abort(400, "Not a JSON")
+    if "email" not in payload:
+        abort(400, "Missing email")
+    if "password" not in payload:
+        abort(400, "Missing password")
+
+    user = User(**payload)
+    user.save()
+
+    return jsonify(user.to_dict()), 201
