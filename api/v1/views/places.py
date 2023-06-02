20c9694943f55e17ed637f0c673dc65d539b028e
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-06-02 12:53:05 +0100

    Add search functionality to places

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,9 +1,12 @@
+from flask import jsonify, abort, request
+from json import JSONDecodeError
+
 from api.v1.views import app_views
 from models import storage
 from models.city import City
 from models.place import Place
 from models.user import User
-from flask import jsonify, abort, request
+from models.state import State
 
 
 @app_views.route("/cities/<city_id>/places")
@@ -140,3 +143,65 @@ def update_place(place_id):
     place.save()
 
     return jsonify(place.to_dict())
+
+
+@app_views.route("/places_search", methods=["POST"])
+def search():
+    # If the HTTP request body is not valid JSON
+    try:
+        guide = request.get_json()
+    except JSONDecodeError:
+        abort(400, "Not a JSON")
+
+    state_ids = guide.get("states")
+    city_ids = guide.get("cities")
+    amenity_ids = guide.get("amenities")
+    result = []
+
+    # If the JSON body is empty or each list of all keys are empty:
+    # retrieve all Place objects
+    if not guide and not state_ids and not city_ids:
+        result = storage.all(Place)
+
+    # If states list is not empty, results should
+    # include all Place objects for each State id listed
+    if state_ids:
+        for state_id in state_ids:
+            state = storage.get(State, state_id)
+            if state:
+                for city in state.cities:
+                    for place in city.places:
+                        result.append(place)
+
+    # If cities list is not empty, results should
+    # include all Place objects for each City id listed
+    if city_ids:
+        for city_id in city_ids:
+            city = storage.get(City, city_id)
+            if city:
+                for place in city.places:
+                    if place not in result:
+                        result.append(place)
+
+    # If amenities list is not empty, limit search results to
+    # only Place objects having all Amenity ids listed
+    if amenity_ids:
+        for place in result:
+            if place.amenities:
+                place_amenity_ids = [amenity.id for amenity in place.amenities]
+                for amenity_id in amenity_ids:
+                    if amenity_id not in place_amenity_ids:
+                        result.remove(place)
+                        break
+
+    # serialize to json
+    result = [storage.get(Place, place.id).to_dict() for place in result]
+    # remove relationship keys with list comprehension and
+    # dictionary comprehension
+    keys_to_remove = ["amenities", "reviews", "amenity_ids"]
+    result = [
+        {k: v for k, v in place_dict.items() if k not in keys_to_remove}
+        for place_dict in result
+    ]
+
+    return jsonify(result)
