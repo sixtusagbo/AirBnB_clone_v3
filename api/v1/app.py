a7b5df510eabd81832298e1baf1d06cdd7391593
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-06-02 04:48:47 +0100

    Setup HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 from os import getenv
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 from api.v1.views import app_views
 from models import storage
@@ -10,7 +11,7 @@ from models import storage
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-
+CORS(app, origins=["0.0.0.0"])
 host = getenv("HBNB_API_HOST", "0.0.0.0")
 port = getenv("HBNB_API_PORT", "5000")
 
