41156230278c0e5dd9965c0d4a94faffdad75eaa
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-06-07 19:38:32 +0100

    Update 404 error

--- api/v1/app.py
+++ api/v1/app.py
@@ -26,7 +26,7 @@ def teardown(exception):
 
 @app.errorhandler(404)
 def not_found(error):
-    return jsonify({"error": "Not Found"}), 404
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
